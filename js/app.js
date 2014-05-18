0850260dc9ee8f9d1fe0a8c9cb3f592daf2ad35e
Daniel Winter <winniwinter@me.com>
2014-05-18 20:44:07 +0200

    added view for sensors
    added charts
    added handlebars as tempting engine

--- js/app.js
+++ js/app.js
@@ -1,28 +1,52 @@
 Sensor = Backbone.Model.extend({
-	defaults: {
-		channel: '',
-		name: '',
-		temperature: 0,
-		tempMin: 0,
-		tempMax: 0
-	}
+    defaults: {
+        channel: '',
+        name: '',
+        temperature: 0,
+        tempMin: 0,
+        tempMax: 0
+    }
 });
 
 SensorList = Backbone.Collection.extend({
-	model: Sensor
+    model: Sensor
 });
 
 TemperatureRequest = Backbone.Model.extend({
-	urlRoot: '/temperatures',
-	defaults: {
-		timestamp: '',
-		logfile: '',
-		sensors: new SensorList()
-	}
+    urlRoot: '/temperatures',
+    defaults: {
+        timestamp: '',
+        logfile: '',
+        sensors: new SensorList()
+    }
 });
 
-$( document ).ready(function() {
-  var temps = new TemperatureRequest();
-  temps.fetch();
-  console.log(temps);
+TemperatureView = Backbone.View.extend({
+
+    initialize: function() {
+        console.log(this.collection);
+        // this.collection.bind("reset", this.render, this);
+    },
+
+    render: function(eventName) {
+        var source = $('#sensor-list-template').html();
+        var template = Handlebars.compile(source);
+        var html = this.template(this.collection.toJSON());
+
+        this.$el.html(html);
+    } 
+});
+
+
+$(document).ready(function() {
+    var temps = new TemperatureRequest();
+    temps.fetch({
+        success: function() {
+            var sensorsCollection = temps.get("sensors");
+
+            var tempView = new TemperatureView({
+                collection: sensorsCollection
+            });
+        }
+    });
 });
